35e5b529237ad687545f80e85958712540e25f02
hanahatem1 <hanahatemm88@gmail.com>
2024-12-07 14:48:14 +0200

    edit

--- lib/const.dart
+++ lib/const.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 
 const kprimaryColor =  Color(0xff637a9f);
 const kFont = 'Lemonada';
-const String geminiApiKey = 'AIzaSyD8jAMWnWwbFZpOFeWlreeOFwvh4q5dXn4';
+//const String geminiApiKey = 'AIzaSyD8jAMWnWwbFZpOFeWlreeOFwvh4q5dXn4';
